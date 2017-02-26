e07f3fe0fc2510db0c14b2c88650dc806fa1bf75
Marc Bintinger <mb@miniserver.local>
2017-02-26 16:50:00 +0100

    Initial Commit

---
+++ HaveYouMetMacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HaveYouMetMacOS
+//
+//  Created by Marc Bintinger on 26.02.17.
+//  Copyright © 2017 Marc Bintinger. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HaveYouMetMacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HaveYouMetMacOS
+//
+//  Created by Marc Bintinger on 26.02.17.
+//  Copyright © 2017 Marc Bintinger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
